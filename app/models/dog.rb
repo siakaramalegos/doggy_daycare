24c8c6968b7278a14661fb0457cdc58a5780fcfa
Sia <siakaramalegos@gmail.com>
2015-05-20 17:10:50 -0500

    Changed dog index to panels with photos

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,6 @@ class Dog < ActiveRecord::Base
   belongs_to :owner
   belongs_to :breed
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_:style.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
