89ffac60168e23de801ffe585a1f148381e4434a
Sia <siakaramalegos@gmail.com>
2015-05-21 15:02:49 -0500

    Annotated models and added validation to dogs, breeds, and owners

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,32 @@ class Dog < ActiveRecord::Base
   belongs_to :owner
   belongs_to :breed
 
+  validates :name, :owner_id, :breed_id, presence: true
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
+
+# == Schema Information
+#
+# Table name: dogs
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  owner_id            :integer
+#  breed_id            :integer
+#  dob                 :date
+#  vet                 :string
+#  vet_phone           :string
+#  in_daycare          :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+# Indexes
+#
+#  index_dogs_on_breed_id  (breed_id)
+#  index_dogs_on_owner_id  (owner_id)
+#
--- test/models/dog_test.rb
+++ test/models/dog_test.rb
@@ -5,3 +5,28 @@ class DogTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: dogs
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  owner_id            :integer
+#  breed_id            :integer
+#  dob                 :date
+#  vet                 :string
+#  vet_phone           :string
+#  in_daycare          :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+# Indexes
+#
+#  index_dogs_on_breed_id  (breed_id)
+#  index_dogs_on_owner_id  (owner_id)
+#
