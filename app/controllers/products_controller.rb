b907717cb627f3884123069c4d4acebe1d4f77ba
Sia <siakaramalegos@gmail.com>
2015-06-02 11:41:41 -0500

    Added note about product delete notification bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,6 +54,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    # TODO:  fix notifications when attempting to destroy a product that has a line item
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
